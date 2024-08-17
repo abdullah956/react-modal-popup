0d9dd4c510d7becd6b0a14c483567d4edf9d795f
abdullah956 <abdullaharshed956@gmail.com>
2024-08-17 11:26:51 +0500

    modalpopup-done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ModalTest from "./components/custom-modal-popup/modal-test";
+function App() {
+  return (
+    <div className="App">
+      <ModalTest />
+    </div>
+  );
+}
+
+export default App;
